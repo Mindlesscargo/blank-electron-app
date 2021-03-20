f3856b835e912db920299f870c300116ba507242
Mindlesscargo <39963404+Mindlesscargo@users.noreply.github.com>
2021-03-20 00:25:55 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,30 @@
+const { app, BrowserWindow, ipcMain } = require('electron')
+const path = require('path')
+
+function createWindow () {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'app', 'renderer', 'scripts', 'preload.js')
+    }
+  })
+
+  win.loadFile(path.join(__dirname, 'app', 'renderer', 'views', 'index.html'))
+}
+
+app.whenReady().then(() => {
+  createWindow()
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow()
+    }
+  })
+})
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
